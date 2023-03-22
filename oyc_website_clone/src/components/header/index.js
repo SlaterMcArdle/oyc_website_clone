de96978e167a3686aefe833152292e5241404f1b
Slater McArdle <slater.mcardle@outlook.com>
2023-03-22 16:40:29 -0700

    React app created and material ui installed

---
+++ oyc_website_clone/src/components/header/index.js
@@ -0,0 +1,19 @@
+import './style.css';
+
+export default function Header() {
+    return (
+        <div class="header-wrap">
+            <div class="header-container">
+                <div class="header-logo">
+                    <a href="#">
+                        <img src={process.env.PUBLIC_URL + "/Oceanside_Yacht_Club_Logo_150x79.jpg"} alt="OYC Logo"/>
+                    </a>
+                </div>
+                <div class="header-content">
+                    <div class="header-text">Call Us Today 760-722-5751</div>
+                </div>
+            </div>
+            
+        </div>
+    )
+};
---
+++ oyc_website_clone/src/pages/home/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+import Header from '../../components/header';
+
+export default function Home() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+};
