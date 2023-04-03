04822de7bdbe57df22a9b805c6fe8f6db07bee96
Slater McArdle <slater.mcardle@outlook.com>
2023-04-02 17:43:20 -0700

    moved header, navbar and footer to app as universal components

--- oyc_website_clone/src/App.js
+++ oyc_website_clone/src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/home';
+import Header from './components/header';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './appStyles';
 
 function App() {
   return (
+    // Overall Material UI Theme
     <ThemeProvider theme={theme}>
       <div className="App">
+        {/* Universal header and navbar */}
+        <Header />
+        <Navbar />
+        {/* Pages */}
         <Home />
+        {/* Universal footer */}
+        <Footer />
       </div>
     </ThemeProvider>
   );
--- oyc_website_clone/src/pages/home/index.js
+++ oyc_website_clone/src/pages/home/index.js
@@ -1,17 +1,12 @@
-import Header from '../../components/header';
-import Navbar from '../../components/navbar';
+
 import Banner from '../../components/banner';
-import Footer from '../../components/footer';
 import Slideshow from '../../components/slideshow';
 
 export default function Home() {
     return (
-        <div>
-            <Header />
-            <Navbar />
+        <>
             <Banner />
             <Slideshow />
-            <Footer />
-        </div>
+        </>
     )
 };
