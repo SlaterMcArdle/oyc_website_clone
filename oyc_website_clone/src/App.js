797d81b3861a3b940059fcd6fb9c561555668bfc
Slater McArdle <slater.mcardle@outlook.com>
2023-04-03 18:24:26 -0700

    working on getting calender working. No success yet.

--- oyc_website_clone/src/App.js
+++ oyc_website_clone/src/App.js
@@ -1,26 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/home';
+import CalendarPage from './pages/calendar';
 import Header from './components/header';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './appStyles';
+import ClubCalendar from './components/calendar';
 
 function App() {
   return (
     // Overall Material UI Theme
-    <ThemeProvider theme={theme}>
-      <div className="App">
+    // <ThemeProvider theme={theme}>
+      <div className="App" style={{minHeight: '500px'}}>
         {/* Universal header and navbar */}
-        <Header />
-        <Navbar />
+        {/* <Header /> */}
+        {/* <Navbar /> */}
         {/* Pages */}
-        <Home />
+        {/* <Home /> */}
+        <ClubCalendar />
+        {/* <CalendarPage /> */}
         {/* Universal footer */}
-        <Footer />
+        {/* <Footer /> */}
       </div>
-    </ThemeProvider>
+    // </ThemeProvider>
   );
 }
 
---
+++ oyc_website_clone/src/pages/calendar/index.js
@@ -0,0 +1,11 @@
+import ClubCalendar from "../../components/calendar";
+
+let CalendarPage = () => {
+    return (
+        <div>
+            <ClubCalendar/>
+        </div>
+    )
+}
+
+export default CalendarPage;
