011c313e61c025dfe1f9f3ffe023a9e8375581fe
Slater McArdle <slater.mcardle@outlook.com>
2023-06-11 17:59:51 -0700

    Got carousel sizing, positioning, and styling mostly working. Need to work on dynamically resizing image width.

--- oyc_website_clone/src/components/slideshow/index.js
+++ oyc_website_clone/src/components/slideshow/index.js
@@ -1,35 +1,73 @@
-import Carousel from 'react-material-ui-carousel';
-import { Paper, Button, Typography } from '@mui/material';
+import Carousel from "react-material-ui-carousel";
+import { Paper, Button, Typography, Grid } from "@mui/material";
+import "./style.css";
 
 let Slideshow = (props) => {
-    let items = [
-        {
-            name: "Random Name #1",
-            description: "Probably the most random thing you have ever seen!"
-        },
-        {
-            name: "Random Name #2",
-            description: "Hello World!"
-        }
-    ]
+  let items = [
+    {
+      image:
+        "https://oceansideyc.net/wp-content/uploads/2023/06/2023-06-02_Bingo_Night.jpg",
+      description: "Oceanside Yacht club Bingo Night",
+    },
+    {
+      image:
+        "https://oceansideyc.net/wp-content/uploads/2023/06/2023-06-02_Bingo_Night.jpg",
+      description: "Oceanside Yacht club Bingo Night",
+    },
+  ];
 
-    return (
-        <Carousel>
-            {
-                items.map((item, i) => <Item key={i} item={item} />)
-            }
-        </Carousel>
-    )
-}
+  return (
+    <Grid container className="slideShowContainer">
+        <Grid xs></Grid>
+        <Grid xs={10} md={6} xl={5}>
+            <Carousel height="400px" sx={{ maxWidth: "100vw" }}>
+            {items.map((item, i) => (
+                <Slide key={i} item={item} />
+            ))}
+            </Carousel>
+        </Grid>
+        <Grid xs></Grid>
+        
+    </Grid>
+  );
+};
 
-let Item = (props) => {
-    return (
-        <Paper>
-            <Typography variant={"h2"}>{props.item.name}</Typography>
-            <Typography variant={"p"}>{props.item.description}</Typography>
-            <Button>Check it out!</Button>
-        </Paper>
-    )
-}
+let Slide = (props) => {
+  return (
+    <div style={{ height: "400px", position: "relative" }}>
+      <Paper>
+        <Grid container spacing={0}>
+          <Grid item xs>
+            <div className="blurContainer">
+              <img
+                className="blurredLeft"
+                src={props.item.image}
+                alt={props.item.description}
+                style={{ objectFit: "fill", maxHeight: "400px" }}
+              />
+            </div>
+          </Grid>
+          <Grid item xs="auto">
+            <img
+              src={props.item.image}
+              alt={props.item.description}
+              style={{ objectFit: "contain", maxHeight: "400px", maxWidth: "100%" }}
+            />
+          </Grid>
+          <Grid item xs>
+            <div className="blurContainer">
+              <img
+                className="blurredRight"
+                src={props.item.image}
+                alt={props.item.description}
+                style={{ objectFit: "fill", maxHeight: "400px" }}
+              />
+            </div>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
+};
 
 export default Slideshow;
